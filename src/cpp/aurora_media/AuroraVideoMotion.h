1a19799ee3f8a0d20d417bb1b2d3fc2a7652ccf5
andreamouren-ctrl <andreamouren@gmail.com>
2026-09-23 16:03:01 +0200

    Add limited-candidate MC8R4 API

--- src/cpp/aurora_media/AuroraVideoMotion.h
+++ src/cpp/aurora_media/AuroraVideoMotion.h
@@ -20,6 +20,12 @@ public:
                                       std::uint32_t width,
                                       std::uint32_t height);
 
+    static MotionResidual encode_mc8r4_limited(ByteView current_yuv420,
+                                               ByteView previous_yuv420,
+                                               std::uint32_t width,
+                                               std::uint32_t height,
+                                               std::size_t max_candidates);
+
     static Bytes decode_mc8r4(ByteView motion_map,
                              ByteView residual_yuv420,
                              ByteView previous_yuv420,
