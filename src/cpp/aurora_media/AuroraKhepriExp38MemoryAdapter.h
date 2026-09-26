0ff9c20a4fbf7b6b96b7409aea4185234d303838
andreamouren-ctrl <andreamouren@gmail.com>
2026-09-26 09:15:35 +0200

    AURORA Media: add experimental EXP-38 memory adapter interface

---
+++ src/cpp/aurora_media/AuroraKhepriExp38MemoryAdapter.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "AuroraCodecInterfaces.h"
+
+namespace aurora::media {
+
+class AuroraKhepriExp38MemoryAdapter final : public IKhepriBackend {
+public:
+    Bytes encode(ByteView input) override;
+    Bytes decode(ByteView input) override;
+};
+
+} // namespace aurora::media
