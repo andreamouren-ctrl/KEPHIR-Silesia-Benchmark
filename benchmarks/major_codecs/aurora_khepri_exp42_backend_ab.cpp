ffebefc9310747535b1ea98c5a9d88a839fed78b
andreamouren-ctrl <andreamouren@gmail.com>
2026-09-26 12:50:47 +0200

    AURORA Media: benchmark EXP42 against actual EXP40 baseline

--- benchmarks/major_codecs/aurora_khepri_exp42_backend_ab.cpp
+++ benchmarks/major_codecs/aurora_khepri_exp42_backend_ab.cpp
@@ -3,9 +3,9 @@
 using Backend = aurora::media::AuroraKhepriExp42MemoryAdapter;
 static constexpr const char* kBackendName = "EXP42_LOG_COST_CACHE";
 #else
-#include "AuroraKhepriExp37MemoryAdapter.h"
-using Backend = aurora::media::AuroraKhepriExp37MemoryAdapter;
-static constexpr const char* kBackendName = "EXP37_DUAL";
+#include "AuroraKhepriExp40MemoryAdapter.h"
+using Backend = aurora::media::AuroraKhepriExp40MemoryAdapter;
+static constexpr const char* kBackendName = "EXP40_SHARED_CACHE";
 #endif
 
 #include "AuroraVideoMotion.h"
